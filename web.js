9978f78f87664cdd535d95a315c15a6dfd39d496
physician <feijj0121@gmail.com>
2013-07-10 20:32:31 +0000

    Update web.js to read from file

--- web.js
+++ web.js
@@ -1,10 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer(fs.readFileSync('index.html'), 'utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString());
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
