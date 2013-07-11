cfb4fda2d5ffb734470e42188bd2721ec18cd3fb
physician <feijj0121@gmail.com>
2013-07-11 03:53:57 +0000

    Updated grader.js to take either a file or a URL as an input on the command line

--- grader.js
+++ grader.js
@@ -22,10 +22,13 @@ References:
 */
 
 var fs = require('fs');
+var util = require('util');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://arcane-taiga-6084.herokuapp.com/";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -61,10 +64,22 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var checkURL = function(url) {
+    rest.get(url).on('complete', function(result) {
+        if (result instanceof Error) {
+        util.puts('Error: ' + result.message);
+        this.retry(5000); // try again after 5 sec
+        } else {
+          util.puts(result);
+        }
+    })
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url', 'Path to url', clone(checkURL), URL_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
